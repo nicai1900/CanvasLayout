8051e19522e3312c253746268fe6249c9604b323
zhengjie <zhengjie1900@qq.com>
2015-05-29 14:11:05 +0800

    Add FrameElement

---
+++ library/src/main/java/com/nicaiya/canvaslayout/library/FrameElement.java
@@ -0,0 +1,126 @@
+package com.nicaiya.canvaslayout.library;
+
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+public class FrameElement extends UIElementGroup {
+
+    private static final String TAG = FrameElement.class.getSimpleName();
+    private static final boolean DEG = false;
+
+    private static final int DEFAULT_CHILD_GRAVITY = Gravity.TOP;
+
+    public FrameElement(UIElementHost host) {
+        super(host);
+    }
+
+    public FrameElement(UIElementHost host, AttributeSet attrs) {
+        super(host, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int count = getElementCount();
+
+        int maxHeight = 0;
+        int maxWidth = 0;
+
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
+        for (int i = 0; i < count; i++) {
+            UIElement child = getElementAt(i);
+            if (child.getVisibility() != View.GONE) {
+                FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) child.getLayoutParams();
+
+                maxWidth = Math.max(maxWidth,
+                        child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin);
+                maxHeight = Math.max(maxHeight,
+                        child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin);
+            }
+        }
+        maxWidth += getPaddingLeft() + getPaddingRight();
+        maxHeight += getPaddingTop() + getPaddingBottom();
+        setMeasuredDimension(resolveSize(maxWidth, widthMeasureSpec), resolveSize(maxHeight, heightMeasureSpec));
+    }
+
+    @Override
+    protected void onLayout(int left, int top, int right, int bottom) {
+        int count = getElementCount();
+
+        final int parentLeft = getPaddingLeft();
+        final int parentRight = getWidth() - getPaddingRight();
+
+        final int parentTop = getPaddingTop();
+        final int parentBottom = getHeight() - getPaddingBottom();
+
+        for (int i = 0; i < count; i++) {
+            UIElement child = getElementAt(i);
+            if (child.getVisibility() != View.GONE) {
+
+                FrameLayout.LayoutParams lp =
+                        (FrameLayout.LayoutParams) child.getLayoutParams();
+
+                final int width = child.getMeasuredWidth();
+                final int height = child.getMeasuredHeight();
+
+                int childLeft;
+                int childTop;
+
+                int gravity = lp.gravity;
+                if (gravity == -1) {
+                    gravity = DEFAULT_CHILD_GRAVITY;
+                }
+
+                final int horizontalGravity = gravity & Gravity.HORIZONTAL_GRAVITY_MASK;
+                final int verticalGravity = gravity & Gravity.VERTICAL_GRAVITY_MASK;
+
+                switch (horizontalGravity & Gravity.HORIZONTAL_GRAVITY_MASK) {
+                    case Gravity.CENTER_HORIZONTAL:
+                        childLeft = parentLeft + (parentRight - parentLeft - width) / 2 +
+                                lp.leftMargin - lp.rightMargin;
+                        break;
+                    case Gravity.RIGHT:
+                        childLeft = parentRight - width - lp.rightMargin;
+                        break;
+                    case Gravity.LEFT:
+                    default:
+                        childLeft = parentLeft + lp.leftMargin;
+                }
+
+                switch (verticalGravity) {
+                    case Gravity.TOP:
+                        childTop = parentTop + lp.topMargin;
+                        break;
+                    case Gravity.CENTER_VERTICAL:
+                        childTop = parentTop + (parentBottom - parentTop - height) / 2 +
+                                lp.topMargin - lp.bottomMargin;
+                        break;
+                    case Gravity.BOTTOM:
+                        childTop = parentBottom - height - lp.bottomMargin;
+                        break;
+                    default:
+                        childTop = parentTop + lp.topMargin;
+                }
+
+                child.layout(childLeft, childTop, childLeft + width, childTop + height);
+            }
+        }
+    }
+
+    @Override
+    protected boolean checkLayoutParams(ViewGroup.LayoutParams lp) {
+        return lp instanceof FrameLayout.LayoutParams;
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateLayoutParams(ViewGroup.LayoutParams lp) {
+        return new FrameLayout.LayoutParams(lp);
+    }
+
+    @Override
+    public ViewGroup.LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new FrameLayout.LayoutParams(getContext(), attrs);
+    }
+}
